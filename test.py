5f6e047c2f0fb5d7a502ce0f9ac89a65668641e1
cow <cow@cowingtonpost.com>
2025-11-14 12:31:32 -0800

    add initial version of code

---
+++ test.py
@@ -0,0 +1,41 @@
+import unittest
+import csv 
+import zscore
+import math
+
+def loadFromFile(filename):
+    test_cases = []
+    with open(filename, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # Skip header
+        for row in reader:
+            test_cases.append(row)
+    return test_cases
+
+def cmp_float(a,b):
+    return math.isclose(a,b, rel_tol=0, abs_tol=1e-8)
+
+def test_case(case):
+    def test(self):
+        x, mu, sigma, expected = case
+        result = zscore.z_score(float(x), float(mu), float(sigma))
+        outcome = cmp_float(float(expected), result)
+        print(f"{"PASS" if outcome else "FAIL"}: x={x} mu={mu}, sigma={sigma}, result={result}, expected={expected}, difference={abs(float(expected) - result)}")
+        self.assertTrue(outcome)
+    
+    return test
+
+class TestZScore(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    test_cases  = loadFromFile("./Zscore - A.csv")
+    test_cases += loadFromFile("./Zscore - B.csv")
+    test_cases += loadFromFile("./Zscore - C.csv")
+    test_cases += loadFromFile("./Zscore - D.csv")
+
+    for (i,t) in enumerate(test_cases):
+        test_name = f"test_{i}"
+        test = test_case(t)
+        setattr(TestZScore, test_name, test)
+    unittest.main()
